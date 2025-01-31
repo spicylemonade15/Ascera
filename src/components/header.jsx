5cc8396794b344d0b082874380f3933281769369
Tuypang Carsco <mailshivanitripathi@gmail.com>
2025-01-31 16:32:32 +0530

    Header Component

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {Button} from './ui/button'
+const Header = () => {
+  return (
+    <div>
+      <nav className='py-4 flex justify-between items-center'>
+        <Link>
+        <img src="/logo.png" className='h-20' />
+        </Link>
+
+        <Button variant="outline">Login</Button>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
